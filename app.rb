eebdb104acee284489e7e0e99b4a91e9b7fbad30
Valera <bomzadua@yomail.info>
2017-03-09 19:41:00 +0200

    commenting functionality moved to AR, new features added

--- app.rb
+++ app.rb
@@ -13,23 +13,18 @@ end
 
 class Comment < ActiveRecord::Base
 	validates :author, presence: true, length: {minimum: 3}
-	validates :content, presence: true, length: {minimum: 55}
+	validates :content, presence: true, length: {minimum: 19}
 end
-
 
 get '/' do
 	erb :index
 end
 
-def db_init
-#@db= SQLite3::Database.new 'forum.db'
-	@db.results_as_hash=true
-end
-
 before do
 	@dataposted=Post.new
-	@posts = Post.all
+	@posts = Post.order('created_at DESC')
 	@comments = Comment.all
+	@comment = Comment.new
 end
 
 get '/newpost' do
@@ -49,28 +44,23 @@ post '/newpost' do
 end
 
 get '/post/:post_id' do
-	post_id=params[:post_id]
-	result=@db.execute 'select * from posts where id=?', [post_id]
-	@comments=@db.execute 'select * from comments where post_id=? order by Id', [post_id]
-	@post=result[0]
+	postid=params[:post_id]
+	@post= Post.find (postid)
+	@comments= Comment.where(post_id: postid).find_each
 
 	erb :post
 end
 
 post '/post/:post_id' do
-	post_id=params[:post_id]
-	@author=params[:author]
-	@content=params[:postContent]
-	#result=@db.execute 'select * from posts where id=?', [post_id]
-	#@post=result[0]
-	if @author.length<1
-		#@error="Enter your name, please!"
-		redirect to ('/post/'+post_id)
+	postid=params[:post_id]
+	@post= Post.find (postid)
+
+@comment=Comment.new params[:comment]
+
+	if @comment.save
+		redirect to ('/post/'+postid)
+	else
+		@error = "An error occurred, the record has not been saved to database! </br>Details: "+@comment.errors.full_messages.first
+		erb :post
 	end
-	if @content.length<1
-		#@error="Type something, please!"
-		redirect to ('/post/'+post_id)
-	end
-@db.execute 'Insert into "comments"(author, content, created_date, post_id) values (?, ?, datetime(), ?)', [@author, @content, post_id]
-	redirect to ('/post/'+post_id)
 end
